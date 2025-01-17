bf9075416f995c8cf8959c68e0b35336e8f59c8e
Carlos Dolz <carlos.dolz@fainascensores.com>
2025-01-17 09:13:41 +0100

    master: Spring Boot project creation

---
+++ spb-backend/src/main/java/com/websample/spb_backend/SpbBackendApplication.java
@@ -0,0 +1,13 @@
+package com.websample.spb_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpbBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpbBackendApplication.class, args);
+	}
+
+}
---
+++ spb-backend/src/test/java/com/websample/spb_backend/SpbBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.websample.spb_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpbBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
